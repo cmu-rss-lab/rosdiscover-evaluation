941abe7332ea748957ea068e8620297f74155440
Tobias Dürschmid <duerschmid@cmu.edu>
2021-07-27 12:36:39 -0400

    Fix run script

--- run_rosdiscover.sh
+++ run_rosdiscover.sh
@@ -1,4 +1,4 @@
 cd ~/rosdiscover
 pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug.yml > ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug_out.yml
-pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug.yml > ~rosdiscover-evaluation/bugs/$1/rd_$1_bug_out.yml
-pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix.yml > ~rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix_out.yml
+pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug.yml > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_out.yml
+pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix.yml > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix_out.yml
