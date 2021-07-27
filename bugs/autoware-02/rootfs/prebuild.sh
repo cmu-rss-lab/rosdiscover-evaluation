641bf090cdb11280e3ed4e7b8af89bbef8bfd29f
Tobias Dürschmid <duerschmid@cmu.edu>
2021-07-27 12:08:21 -0400

    Fix prebuild

--- bugs/autoware-02/rootfs/prebuild.sh
+++
@@ -1,3 +0,0 @@
-sed -i 's/\.\//\/pre_bug\/src\/repo\/ros\//' /pre_bug/src/repo/ros/src/util/packages/model_publisher/launch/vehicle_model.launch
-sed -i 's/\.\//\/bug\/src\/repo\/ros\//' /bug/src/repo/ros/src/util/packages/model_publisher/launch/vehicle_model.launch
-sed -i 's/\.\//\/bug_fix\/src\/repo\/ros\//' /bug_fix/src/repo/ros/src/util/packa/ges/model_publisher/launch/vehicle_model.launch
