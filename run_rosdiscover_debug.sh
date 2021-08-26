77720315fef0531c5656d050e52e06a872bfb53d
Tobias Dürschmid <duerschmid@cmu.edu>
2021-08-25 21:33:04 -0400

    Fix run_rosdiscover_debug.sh

--- run_rosdiscover_debug.sh
+++ run_rosdiscover_debug.sh
@@ -1,4 +1,4 @@
 pushd ~/rosdiscover
-pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug.yml >& sed 's/\x1b\[[0-9;]*m//g' >  ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug_out.log
-pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug.yml >& sed 's/\x1b\[[0-9;]*m//g' > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_out.log
-pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix.yml >& sed 's/\x1b\[[0-9;]*m//g' > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix_out.log
+pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug.yml |& sed 's/\x1b\[[0-9;]*m//g' >  ~/rosdiscover-evaluation/bugs/$1/rd_$1_pre_bug_out.log
+pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug.yml |& sed 's/\x1b\[[0-9;]*m//g' > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_out.log
+pipenv run rosdiscover launch ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix.yml |& sed 's/\x1b\[[0-9;]*m//g' > ~/rosdiscover-evaluation/bugs/$1/rd_$1_bug_fix_out.log
