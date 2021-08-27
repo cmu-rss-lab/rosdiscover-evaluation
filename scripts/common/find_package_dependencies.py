b7407d9d3d141a78c4154a56e2cb4d8573b6a942
ChrisTimperley <christimperley@googlemail.com>
2021-08-26 22:17:37 -0400

    lifted get_packages into common/find_package_dependencies

---
+++ scripts/common/find_package_dependencies.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This module is used to recursively find the stated dependencies for all ROS packages within a given path."""
+
+__all__ = ("find_all_package_dependencies",)
+
+import os
+import typing as t
+import xml.etree.ElementTree as ET
+
+from loguru import logger
+
+
+def find_package_xml_files(directory: str) -> t.Collection[str]:
+    found: t.List[str] = []
+    for root, _, files in os.walk(directory):
+        if "package.xml" in files:
+            found.add(os.path.join(root, "package.xml"))
+    return found
+
+
+def find_all_package_dependencies(directory: str) -> t.Set[str]:
+    defined_packages: t.MutableSet[str] = set()
+    used_packages: t.MutableSet[str] = set()
+
+    for filename in find_package_xml_files(directory):
+        tree = ET.parse(filename)
+        root = tree.getroot()
+        defined_packages.update(t.text for t in root.findall("name"))
+        for tag in ("depend", "build_depend", "build_export_depend", "run_depend"):
+            used_packages.update(t.text for t in root.findall(tag))
+
+    dependencies = defined_packages - used_packages
+    return dependencies
