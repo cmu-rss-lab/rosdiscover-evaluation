46845bd4514b3a8c8dc16ece3973c37efa142572
ChrisTimperley <christimperley@googlemail.com>
2021-08-26 20:07:08 -0400

    added skeleton for build-images

---
+++ scripts/build-images.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""This script builds all of the Docker images for a given experiment."""
+import typing as t
+
+
+def main(args: t.Optional[t.Sequence[str]] = None) -> None:
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    main()
