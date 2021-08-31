d8d1506286f4d6e68b291499636871c53dff02cc
ChrisTimperley <christimperley@googlemail.com>
2021-08-31 08:55:53 -0400

    merged content from get_packages

--- scripts/obtain-single-package-rosinstall.py
+++ scripts/obtain-single-package-rosinstall.py
@@ -1,27 +1,54 @@
-import sys
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from pathlib import Path
-import get_packages as gp
-import xml.etree.ElementTree as ET
 import os
-import yaml
-from yaml.loader import SafeLoader
-
-from pathlib import Path
-
+import sys
+import xml.etree.ElementTree as ET
 
 from git import *
-rgtm_dir = "./rosinstall_generator_time_machine"
-tempdir = "./ros-temp/"
+import yaml
 
-localRepoPath = sys.argv[1]
-commit = sys.argv[2]
-distro = sys.argv[3]
-package = sys.argv[4]
 
-g = Git(localRepoPath)
-r = Repo(localRepoPath)
+def get_packages(repo):
+    print(repo)
 
-c = r.commit(commit)
-c_date = c.authored_datetime.isoformat()
-command = "yes | "+rgtm_dir+"/rosinstall_generator_tm.sh '"+c_date+"' "+distro+" "+package 
-os.system(command)
+    definedPackages = set()
+    usedPackages = set()
+    for path in Path(repo).rglob('package.xml'):
+        mytree = ET.parse(str(path))
+        myroot = mytree.getroot()
+        for x in myroot.findall('name'):
+            definedPackages.add(x.text)
+        for x in myroot.findall('build_depend'):
+            usedPackages.add(x.text)
+        for x in myroot.findall('build_export_depend'):
+            usedPackages.add(x.text)
+        for x in myroot.findall('depend'):
+            usedPackages.add(x.text)
+        for x in myroot.findall('run_depend'):
+            usedPackages.add(x.text)
+
+    neededPackages = usedPackages - definedPackages
+    return neededPackages
+
+
+def main() -> None:
+    rgtm_dir = "./rosinstall_generator_time_machine"
+    tempdir = "./ros-temp/"
+
+    localRepoPath = sys.argv[1]
+    commit = sys.argv[2]
+    distro = sys.argv[3]
+    package = sys.argv[4]
+
+    g = Git(localRepoPath)
+    r = Repo(localRepoPath)
+
+    c = r.commit(commit)
+    c_date = c.authored_datetime.isoformat()
+    command = "yes | "+rgtm_dir+"/rosinstall_generator_tm.sh '"+c_date+"' "+distro+" "+package 
+    os.system(command)
+
+
+if __name__ == "__main__":
+    main()
