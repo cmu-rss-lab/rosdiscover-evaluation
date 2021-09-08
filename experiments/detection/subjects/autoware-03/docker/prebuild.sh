dc4bddddd409f93e0888a339aa8d030cf9fa161c
Tobias Dürschmid <duerschmid@cmu.edu>
2021-09-08 13:29:04 -0400

    Try to fix husky

--- experiments/detection/subjects/autoware-03/docker/prebuild.sh
+++ experiments/detection/subjects/autoware-03/docker/prebuild.sh
@@ -1,1 +1,6 @@
-sed -i 's/\.\//\/ros_ws\/src\/autoware\/ros\//' /ros_ws/src/autoware/ros/src/util/packages/model_publisher/launch/vehicle_model.launch
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2.7
+
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
+sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
+sudo apt-get update -y
+sudo apt-get install -y --no-install-recommends cuda
