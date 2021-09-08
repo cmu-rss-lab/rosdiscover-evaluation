2a8cbbf9d552bd5dc905a323214f3d51182df3ed
Tobias Dürschmid <duerschmid@cmu.edu>
2021-09-08 11:37:16 -0400

    Add autoware experiment configs

---
+++ experiments/detection/subjects/autoware-01/docker/prebuild.sh
@@ -0,0 +1,5 @@
+cd /
+mkdir .autoware
+cd .autoware
+wget https://autoware-ai.s3.us-east-2.amazonaws.com/sample_moriyama_data.tar.gz
+tar zxfv sample_moriyama_data.tar.gz
---
+++ experiments/detection/subjects/autoware-13/docker/preinstall.sh
@@ -0,0 +1,1 @@
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2.7
