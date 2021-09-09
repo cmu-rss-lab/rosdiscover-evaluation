757bb43501610cc6e13d8872bd3b0965b1401f85
Tobias Dürschmid <duerschmid@cmu.edu>
2021-09-09 14:51:31 -0400

    Fix autoware experiment config

--- experiments/detection/subjects/autoware-08/docker/prebuild_.sh
+++ experiments/detection/subjects/autoware-08/docker/prebuild_.sh
@@ -1,6 +1,5 @@
-curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2.7
-
 wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_6.5-14_amd64.deb
 sudo apt-get update -y
 sudo apt-get install -y --no-install-recommends cuda
+
