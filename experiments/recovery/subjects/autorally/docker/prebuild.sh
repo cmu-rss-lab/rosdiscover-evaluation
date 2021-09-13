d0d577ee6aecb09d6f5a1d13d661c37498275c6a
Tobias Dürschmid <duerschmid@cmu.edu>
2021-09-13 16:57:29 -0400

    Update experiment configs

--- experiments/recovery/subjects/autorally/docker/prebuild.sh
+++ experiments/recovery/subjects/autorally/docker/prebuild.sh
@@ -3,15 +3,6 @@ set -eu
 
 EIGEN_VERSION="3.3.7"
 
-echo "installing cuda"
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
-sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
-sudo apt-get update
-sudo apt-get -y install cuda
-echo "installed cuda"
-
 echo "installing geographiclib"
 cd /tmp
 wget -nv https://sourceforge.net/projects/geographiclib/files/distrib/GeographicLib-1.50.1.tar.gz
