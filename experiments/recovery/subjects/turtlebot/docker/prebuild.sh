237f1519775b18c6003010a78cb93c649a802b22
ChrisTimperley <christimperley@googlemail.com>
2021-09-22 19:27:33 -0400

    added manual libfreenect install for turtlebot

--- experiments/recovery/subjects/turtlebot/docker/prebuild.sh
+++ experiments/recovery/subjects/turtlebot/docker/prebuild.sh
@@ -5,3 +5,14 @@ set -eu
 # - https://github.com/ros/geometry/issues/144
 # - https://github.com/ros/ros_comm/pull/962
 sed -i "s#^  INCLUDE_DIRS include\$#  INCLUDE_DIRS include include/xmlrpcpp#" /ros_ws/src/ros_comm/xmlrpcpp/CMakeLists.txt
+
+# see: https://answers.ros.org/question/313893/unabe-to-install-freenect-in-ros-melodicubuntu-1804/
+git clone https://github.com/OpenKinect/libfreenect.git /tmp/libfreenect
+cd /tmp/libfreenect
+git checkout v0.5.5
+mkdir build
+cd build
+cmake ..
+make
+make install
+rm -rf /tmp/*
