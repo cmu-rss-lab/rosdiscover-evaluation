6f7fbac489161cd5d7efaf6d98b5ac9d7e185428
ChrisTimperley <christimperley@googlemail.com>
2021-09-22 21:19:56 -0400

    added fix for missing nav_msgs in turtlebot

--- experiments/recovery/subjects/turtlebot/docker/prebuild.sh
+++ experiments/recovery/subjects/turtlebot/docker/prebuild.sh
@@ -6,6 +6,9 @@ set -eu
 # - https://github.com/ros/ros_comm/pull/962
 sed -i "s#^  INCLUDE_DIRS include\$#  INCLUDE_DIRS include include/xmlrpcpp#" /ros_ws/src/ros_comm/xmlrpcpp/CMakeLists.txt
 
+# fix another broken CMakeLists.txt
+sed -i "4inav_msgs" src/kobuki/kobuki_auto_docking/CMakeLists.txt
+
 # see: https://answers.ros.org/question/313893/unabe-to-install-freenect-in-ros-melodicubuntu-1804/
 git clone https://github.com/OpenKinect/libfreenect.git /tmp/libfreenect
 cd /tmp/libfreenect
