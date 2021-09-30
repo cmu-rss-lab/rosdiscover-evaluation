b5dde04d2927963d6452040613b797047fe13da2
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-09-30 16:39:22 -0400

    fixed some bugs

--- scripts/compare_recovered_observed.py
+++ scripts/compare_recovered_observed.py
@@ -17,8 +17,10 @@ def compare_sets(
     observed: t.Set[t.Tuple[str, str]],
     recovered: t.Set[t.Tuple[str, str]],
 ) -> str:
-    observed_not_recovered = observed.difference(recovered)
-    recovered_not_observed = recovered.difference(observed)
+    observed_not_recovered = list(observed.difference(recovered))
+    recovered_not_observed = list(recovered.difference(observed))
+    observed_not_recovered.sort(key=lambda x: x[0])
+    recovered_not_observed.sort(key=lambda x: x[0])
 
     differences = f"\n{kind} in observed that aren't in recovered: "
     differences += ", ".join(f"{o[0]}{joiner}{o[1]}" for o in observed_not_recovered)
@@ -26,6 +28,29 @@ def compare_sets(
     differences += ", ".join(f"{o[0]}{joiner}{o[1]}" for o in recovered_not_observed)
 
     return differences
+
+
+def remove_actions_from_recovered(recovered_publishers, recovered_subscribers, recovered_action_clients,
+                                  recovered_action_servers):
+    for action_info in recovered_action_clients:
+        node_name = action_info[0]
+        action = action_info[1]
+
+        recovered_publishers.remove((node_name, f"{action}/result"))
+        recovered_publishers.remove((node_name, f"{action}/feedback"))
+        recovered_publishers.remove((node_name, f"{action}/status"))
+        recovered_subscribers.remove((node_name, f"{action}/goal"))
+        recovered_subscribers.remove((node_name, f"{action}/cancel"))
+
+    for action_info in recovered_action_servers:
+        node_name = action_info[0]
+        action = action_info[1]
+
+        recovered_subscribers.remove((node_name, f"{action}/result"))
+        recovered_subscribers.remove((node_name, f"{action}/feedback"))
+        recovered_subscribers.remove((node_name, f"{action}/status"))
+        recovered_publishers.remove((node_name, f"{action}/goal"))
+        recovered_publishers.remove((node_name, f"{action}/cancel"))
 
 
 def compare(config: ExperimentConfig) -> None:
@@ -54,32 +79,64 @@ def compare(config: ExperimentConfig) -> None:
 
         f.write(compare_sets("Nodes", "/", observed_node_names, recovered_node_names))
 
-        observed_publishers, observed_subscribers, observed_providers = extract_architecture_elements(
-            observed_architecture)
-        recovered_publishers, recovered_subscribers, recovered_providers = extract_architecture_elements(
-            recovered_architecture)
+        observed_publishers, observed_subscribers, observed_providers, observed_action_clients, observed_action_servers\
+            = extract_architecture_elements(observed_architecture)
 
+        recovered_publishers, recovered_subscribers, recovered_providers, recovered_action_clients, \
+            recovered_action_servers = extract_architecture_elements(recovered_architecture)
+
+        remove_actions_from_recovered(recovered_publishers, recovered_subscribers, recovered_action_clients,
+                                      recovered_action_servers)
         f.write(compare_sets("Publishers", "->", observed_publishers, recovered_publishers))
         f.write(compare_sets("Subscribers", "<-", observed_subscribers, recovered_subscribers))
         f.write(compare_sets("Providers", ":", observed_providers, recovered_providers))
+        f.write(compare_sets("Action Servers", "^-", observed_action_servers, recovered_action_servers))
+        f.write(compare_sets("Action Clients", "-^", observed_action_clients, recovered_action_clients))
         f.write("\nCannot observer service clients")
+
+
+def include_element(element: str) -> bool:
+    if element.endswith("parameter_updates"):
+        return False
+    if element.endswith("parameter_descriptions"):
+        return False
+    if element.endswith("set_parameter"):
+        return False
+    return True
 
 
 def extract_architecture_elements(
     architecture: t.List[t.Any]
-) -> t.Tuple[t.Set[t.Tuple[str, str]], t.Set[t.Tuple[str, str]], t.Set[t.Tuple[str, str]]]:
+) -> t.Tuple[
+    t.Set[t.Tuple[str, str]],
+    t.Set[t.Tuple[str, str]],
+    t.Set[t.Tuple[str, str]],
+    t.Set[t.Tuple[str, str]],
+    t.Set[t.Tuple[str, str]],
+]:
     publishers: t.Set[t.Tuple[str, str]] = set()
     subscribers: t.Set[t.Tuple[str, str]] = set()
     providers: t.Set[t.Tuple[str, str]] = set()
-    for node in architecture:
-        for topic in node["pubs"]:
-            publishers.add((node["name"], topic["name"]))
-        for topic in node["subs"]:
-            subscribers.add((node["name"], topic["name"]))
-        for service in node["provides"]:
-            providers.add((node["name"], service["name"]))
+    action_clients: t.Set[t.Tuple[str, str]] = set()
+    action_servers: t.Set[t.Tuple[str, str]] = set()
 
-    return publishers, subscribers, providers
+    for node in architecture:
+        node_name = node["name"]
+        for topic in node["pubs"]:
+            if include_element(topic["name"]):
+                publishers.add((node_name, topic["name"]))
+        for topic in node["subs"]:
+            if include_element((topic["name"])):
+                subscribers.add((node_name, topic["name"]))
+        for service in node["provides"]:
+            if include_element(service["name"]):
+                providers.add((node_name, service["name"]))
+        for client in node["action-clients"]:
+            action_clients.add((node_name, client["name"]))
+        for server in node["action-servers"]:
+            action_servers.add((node_name, server["name"]))
+
+    return publishers, subscribers, providers, action_clients, action_servers
 
 
 def error(message: str) -> t.NoReturn:
