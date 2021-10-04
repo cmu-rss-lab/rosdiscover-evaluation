4dd5c53274d24532b5e436cb9354bd2de175cd7c
ChrisTimperley <christimperley@googlemail.com>
2021-10-04 13:21:53 -0400

    added missing python packages to prebuild for autorally

--- experiments/recovery/subjects/autorally/docker/prebuild.sh
+++ experiments/recovery/subjects/autorally/docker/prebuild.sh
@@ -4,6 +4,16 @@ set -eu
 EIGEN_VERSION="3.3.7"
 GTSAM_VERSION=2c44ee459bc8090364ca8034e2988d3e8a01c422
 
+# ensure that the python environment is correctly configured
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# install necessary python packages
+pip install \
+  empy==3.3.4
+  numpy==1.16.6 \
+
 # geographiclib
 echo "installing geographiclib"
 cd /tmp
