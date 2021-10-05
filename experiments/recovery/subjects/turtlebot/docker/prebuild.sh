050deabfe9da5d5715756ea41b4899a6abe2c622
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-05 14:01:36 -0400

    turtlebot build

--- experiments/recovery/subjects/turtlebot/docker/prebuild.sh
+++ experiments/recovery/subjects/turtlebot/docker/prebuild.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 set -eu
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+# eval "$(pyenv virtualenv-init -)"
+
+## install necessary python packages
+#pip install \
+#  sip==4.19.18
 
 # see
 # - https://github.com/ros/geometry/issues/144
@@ -19,3 +26,15 @@ cmake ..
 make
 make install
 rm -rf /tmp/*
+
+#cd /tmp
+#wget https://www.riverbankcomputing.com/static/Downloads/sip/4.19.25/sip-4.19.25.tar.gz
+#tar zxf sip-4.19.25.tar.gz
+#cd sip-4.19.25
+#python configure.py
+#make install
+#rm -rf /tmp/*
+
+#cd /tmp
+#wget https://files.pythonhosted.org/packages/19/5a/c50a0ccd211f864972da0f2e101722508ffb4e008347fd2a56a99beab9d0/sip-4.19.2-cp35-cp35m-manylinux1_x86_64.whl
+#pip install sip-4.19.2-cp35-cp35m-manylinux1_x86_64.whl
