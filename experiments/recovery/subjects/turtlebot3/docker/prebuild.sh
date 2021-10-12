5271a3c84a362e2dc670ff961694155b5646548d
Tobias Dürschmid <duerschmid@cmu.edu>
2021-10-12 16:53:19 -0400

    Update turtlebot3 configs

--- experiments/recovery/subjects/turtlebot3/docker/prebuild.sh
+++ experiments/recovery/subjects/turtlebot3/docker/prebuild.sh
@@ -9,3 +9,5 @@ pip install \
   numpy==1.16.6 \
   defusedxml==0.6.0 \
   netifaces==0.10.7
+
+sed -i 's/while(!ros::service::call("static_map", req, resp))/ros::Publisher service_pub_ = nh_.advertise<nav_msgs::GetMap::Request>("static_map", 2, true);\nfor(service_pub_.publish(req); !ros::service::call("static_map", req, resp); service_pub_.publish(req))/' /ros_ws/src/navigation/amcl/src/amcl_node.cpp
