5384acf8d2fe753f1fd52aa86488e1f1d48f36dd
Bradley Schmerl <schmerl@cs.cmu.edu>
2021-10-28 09:20:37 -0400

    Added some topics to ignore by default

--- scripts/common/acme.py
+++ scripts/common/acme.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import tempfile
 
 from loguru import logger
 
@@ -7,6 +8,17 @@ import rosdiscover
 import rosdiscover.cli
 
 from .config import ROSDiscoverConfig
+
+
+TOPICS_TO_IGNORE = """/clock
+/tf
+/tf_static
+/diagnostics
+*/load_nodelet
+*/unload_nodelet
+*/bond
+*/list_controllers
+"""
 
 
 def generate_and_check_acme(
@@ -19,10 +31,15 @@ def generate_and_check_acme(
     with ROSDiscoverConfig.create_temporary({
         "image": image,
     }) as config_filename:
+        ignore_file: str = tempfile.mkstemp(suffix="ignore.txt")[1]
+        with open(ignore_file, 'w') as fh:
+            fh.write(TOPICS_TO_IGNORE)
+
         args = ["acme", config_filename]
         args += ["--from-yml", input_filename]
         args += ["--acme", output_filename]
         args += ["--check"]
+        args += ["--ignore-list", ignore_file]
         file_logger = logger.add(log_filename, level="DEBUG")
         logger.debug(f"Calling rosdiscover: {args}")
         try:
@@ -30,5 +47,7 @@ def generate_and_check_acme(
         except Exception:
             logger.exception(f"Failed to convert yml file to Acme for [{image}] captured in [{input_filename}]")
         finally:
+            if ignore_file:
+                os.remove(ignore_file)
             logger.remove(file_logger)
 
