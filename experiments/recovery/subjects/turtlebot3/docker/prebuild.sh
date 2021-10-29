2ff17614e5a3cf8468a3a3fdce4fbaebcf078ba9
Tobias Dürschmid <duerschmid@cmu.edu>
2021-10-29 15:24:29 -0400

    Add code changes to prebuild

--- experiments/recovery/subjects/turtlebot3/docker/prebuild.sh
+++ experiments/recovery/subjects/turtlebot3/docker/prebuild.sh
@@ -11,3 +11,8 @@ pip install \
   netifaces==0.10.7
 
 sed -i 's/while(!ros::service::call("static_map", req, resp))/ros::Publisher service_pub_ = nh_.advertise<nav_msgs::GetMap::Request>("static_map", 2, true);\nfor(service_pub_.publish(req); !ros::service::call("static_map", req, resp); service_pub_.publish(req))/' /ros_ws/src/navigation/amcl/src/amcl_node.cpp
+sed -i 's/private_nh_.param("use_map_topic", use_map_topic_, false);/private_nh_.param("use_map_topic", use_map_topic_, true);/' /ros_ws/src/navigation/amcl/src/amcl_node.cpp
+#sed -i 's/r.sleep();/\/\/r.sleep();/' /ros_ws/src/navigation/move_base/src/move_base.cpp
+#sed -i 's/current_time.toSec() - global_pose.stamp_.toSec() > transform_tolerance_/false/' /ros_ws/src/navigation/costmap_2d/src/costmap_2d_ros.cpp
+sed -i 's/if(as_->isNewGoalAvailable()){/if(as_->isNewGoalAvailable()){\nROS_WARN("[move_base] isNewGoalAvailable=true");/' /ros_ws/src/navigation/move_base/src/move_base.cpp
+sed -i 's/if(as_->isPreemptRequested()){/if(as_->isPreemptRequested()){\nROS_WARN("[move_base] isPreemptRequested=true");/' /ros_ws/src/navigation/move_base/src/move_base.cpp
