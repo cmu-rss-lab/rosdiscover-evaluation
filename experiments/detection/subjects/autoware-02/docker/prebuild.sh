8a3f7615290230d5934cc505671f7854adb6a953
Tobias Dürschmid <duerschmid@cmu.edu>
2021-11-05 22:34:40 -0400

    Add pip prebuild to autoware

--- experiments/detection/subjects/autoware-02/docker/prebuild.sh
+++ experiments/detection/subjects/autoware-02/docker/prebuild.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+set -eu
+
 sed -i 's/\.\//\/ros_ws\/src\/autoware\/ros\//' /ros_ws/src/autoware/ros/src/util/packages/model_publisher/launch/vehicle_model.launch
+
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+## install necessary python packages
+pip install \
+  empy==3.3.4 \
+  numpy==1.16.6 \
+  cheetah==2.4.4 \
+  defusedxml==0.6.0 \
+  netifaces==0.10.7
--- experiments/detection/subjects/autoware-05/docker/prebuild.sh
+++ experiments/detection/subjects/autoware-05/docker/prebuild.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 set -eu
-
-cd /
-mkdir .autoware
-cd .autoware
-wget https://autoware-ai.s3.us-east-2.amazonaws.com/sample_moriyama_data.tar.gz
-tar zxfv sample_moriyama_data.tar.gz
-
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
