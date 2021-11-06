fb7ce92454d19a10bf6af424a360b192a44b1086
Tobias Dürschmid <duerschmid@cmu.edu>
2021-11-05 22:21:21 -0400

    Adjust prebuild for autorally-07

--- experiments/detection/subjects/autorally-07/docker/prebuild.sh
+++ experiments/detection/subjects/autorally-07/docker/prebuild.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 set -eu
 
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+## install necessary python packages
+pip install \
+  empy==3.3.4 \
+  numpy==1.16.6 \
+  cheetah==2.4.4 \
+  defusedxml==0.6.0 \
+  netifaces==0.10.7
+
 echo "installing geographiclib"
 cd /tmp
-wget -nv https://sourceforge.net/projects/geographiclib/files/distrib/GeographicLib-1.48.tar.gz
+wget -nv https://sourceforge.net/projects/geographiclib/files/distrib/GeographicLib-1.48.tar.gz --no-check-certificate
 tar xfpz GeographicLib-1.48.tar.gz
 cd GeographicLib-1.48
 mkdir build
