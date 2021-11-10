1c675cfa3ba2d4e380ec1d2cb21d6edbc41c9b13
ChrisTimperley <christimperley@googlemail.com>
2021-11-10 11:52:15 -0500

    fixed husky_01 image

--- experiments/detection/subjects/husky-01/docker/prebuild.sh
+++ experiments/detection/subjects/husky-01/docker/prebuild.sh
@@ -10,3 +10,6 @@ pip install \
   cheetah==2.4.4 \
   defusedxml==0.6.0 \
   netifaces==0.10.7
+
+# _gencpp has been replaced by _generate_messages_cpp
+find /ros_ws/src -name CMakeLists.txt -print | xargs -n1 sed -i "s#_gencpp#_generate_messages_cpp#g"
