c9447296b6e4307fb18dcb76825d48b8829b9b23
ChrisTimperley <christimperley@googlemail.com>
2021-12-30 15:05:43 -0500

    added placeholder setup.sh to docker dir

---
+++ docker/setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -eu
+
+HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ROOT_DIR="${HERE_DIR}/.."
+
+# TODO ensure that the user has Docker installed
+
+# TODO ensure that the version of Docker is recent enough; if not, produce a warning
+
+docker build -f Dockerfile.runner -t rosqual/icsa22-evaluation:runner "${ROOT_DIR}"
