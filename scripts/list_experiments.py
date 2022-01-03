f8b675c5be58a73f6c521e068104a138e67ec036
Bradley Schmerl <schmerl@cs.cmu.edu>
2022-01-03 13:15:32 -0500

    Fixed path problems.

--- scripts/list_experiments.py
+++ scripts/list_experiments.py
@@ -5,12 +5,13 @@ import os
 def main():
     print("Available experiments and systems:")
     for experiment in ("detection", "recovery"):
-        print(f"  Systems under '{experiment}':\n")
+        print(f"  Systems under '{experiment}':")
         subject_dir = os.path.join(os.path.dirname(__file__), '../experiments', experiment, 'subjects')
         for directory in os.listdir(subject_dir):
-            if os.path.isdir(os.path.join(subject_dir, experiment)) and \
-               os.path.isfile(os.path.join(subject_dir, experiment, "experiment.yml")):
-                print(f"    {directory}\n")
+            isdir = os.path.isdir(os.path.join(subject_dir, directory))
+            contains_experiment_yml = os.path.isfile(os.path.join(subject_dir, directory, "experiment.yml"))
+            if isdir and contains_experiment_yml:
+                print(f"    {directory}")
 
 
 if __name__ == "__main__":
