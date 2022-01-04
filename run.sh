db6bfffe26d042115e4674647cdd58bc17d4e855
Bradley Schmerl <schmerl@cs.cmu.edu>
2022-01-04 10:20:06 -0500

    manipulation of DOCKER_HOST

--- run.sh
+++ run.sh
@@ -10,6 +10,10 @@ if [ -z "$DOCKER_HOST" ]; then
   echo "Setting DOCKER_HOST to //var/run/docker.sock - assuming you are using a global docker install"
   DOCKER_HOST="//var/run/docker.sock"
 fi
-
-docker run -v $DOCKER_HOST:/var/run/docker.sock -v "$current_dir/experiments":/opt/rosdiscover/evaluation/experiments \
+if [[ $DOCKER_HOST == unix:* ]]; then
+  docker_host="${DOCKER_HOST:7}"
+else
+  docker_host="${DOCKER_HOST}"
+fi
+docker run -v "$docker_host":/var/run/docker.sock -v "$current_dir/experiments":/opt/rosdiscover/evaluation/experiments \
  -v "$current_dir/cache/":/root/.roswire rosdiscover/evaluation "$@"
