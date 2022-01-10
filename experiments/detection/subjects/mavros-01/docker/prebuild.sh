72b3c7059842f1a5fc20453a6c912275b3d07b5c
Tobias Dürschmid <duerschmid@cmu.edu>
2022-01-10 17:32:13 -0500

    Add MAVROS experiments

---
+++ experiments/detection/subjects/mavros-01/docker/prebuild.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -eu
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+## install necessary python packages
+pip install \
+  empy==3.3.4 \
+  numpy==1.16.6 \
+  cheetah==2.4.4 \
+  defusedxml==0.6.0 \
+  future==0.17.1 \
+  netifaces==0.10.7
