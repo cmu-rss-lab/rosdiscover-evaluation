8c5f0530604ecdf5ab948697ef43071ea0961210
ChrisTimperley <christimperley@googlemail.com>
2022-01-11 15:58:20 -0500

    setup.sh: install cxx recover

--- docker/setup.sh
+++ docker/setup.sh
@@ -3,9 +3,18 @@ set -eu
 
 HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR="${HERE_DIR}/.."
+ROSDISCOVER_CXX_DIR="${ROOT_DIR}/deps/rosdiscover-cxx-recover"
 
 # TODO ensure that the user has Docker installed
 
+# TODO ensure that the user has make installed
+
+# TODO ensure that the user has jq installed
+
 # TODO ensure that the version of Docker is recent enough; if not, produce a warning
 
+echo "installing rosdiscover experiment runner image..."
 docker build -f "${HERE_DIR}/Dockerfile.runner" -t rosqual/icsa22-evaluation:runner "${ROOT_DIR}"
+
+echo "installing rosdiscover C++ recovery volume..."
+make -C "${ROSDISCOVER_CXX_DIR}/docker"
