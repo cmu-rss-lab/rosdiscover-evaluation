065d27275290a5936824ed68ad9a145bc167b41e
Bradley Schmerl <schmerl@cs.cmu.edu>
2022-01-10 20:49:16 -0500

    added more things to be installed to make rostopic work.

--- experiments/recovery/subjects/husky/docker/postbuild.sh
+++ experiments/recovery/subjects/husky/docker/postbuild.sh
@@ -3,7 +3,7 @@ set -eu
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 
-pip install pycryptodome
+pip install pycryptodome pycryptodomex gnupg
 
 sed -i "30i <arg name=\"initial_pose_a\" value=\"0.0\"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
 sed -i "30i <arg name=\"initial_pose_y\" value=\"0.0\"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
