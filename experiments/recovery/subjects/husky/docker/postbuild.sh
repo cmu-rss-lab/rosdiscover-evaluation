7af7a491f8915d4f9490f2566813c8ff39e5eda9
Bradley Schmerl <schmerl@cs.cmu.edu>
2022-01-10 19:01:00 -0500

    Added postbuild.sh to install required pacakge and set default position.

--- experiments/recovery/subjects/husky/docker/postbuild.sh
+++ experiments/recovery/subjects/husky/docker/postbuild.sh
@@ -5,6 +5,6 @@ eval "$(pyenv init -)"
 
 pip install pycryptodome
 
-sed -i "30i <arg name="initial_pose_a" value="0.0"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
-sed -i "30i <arg name="initial_pose_y" value="0.0"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
-sed -i "30i <arg name="initial_pose_x" value="0.0"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
+sed -i "30i <arg name=\"initial_pose_a\" value=\"0.0\"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
+sed -i "30i <arg name=\"initial_pose_y\" value=\"0.0\"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
+sed -i "30i <arg name=\"initial_pose_x\" value=\"0.0\"/>" /ros_ws/src/husky/husky_navigation/launch/amcl_demo.launch
