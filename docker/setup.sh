fc0886658e9a8c4324588cd17d3d8811a87f1d31
ChrisTimperley <christimperley@googlemail.com>
2022-01-13 21:14:16 -0500

    tidied setup.sh

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,13 +5,14 @@ HERE_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 ROOT_DIR="${HERE_DIR}/.."
 ROSDISCOVER_CXX_DIR="${ROOT_DIR}/deps/rosdiscover-cxx-recover"
 
-# TODO ensure that the user has Docker installed
+function report_error {
+  echo $1
+  exit 1
+}
 
-# TODO ensure that the user has make installed
-
-# TODO ensure that the user has jq installed
-
-# TODO ensure that the version of Docker is recent enough; if not, produce a warning
+which docker &> /dev/null || report_error "failed to find command: docker"
+which make &> /dev/null || report_error "failed to find command: make"
+which jq &> /dev/null || report_error "failed to find command: jq"
 
 echo "installing rosdiscover experiment runner image..."
 docker build -f "${HERE_DIR}/Dockerfile.runner" -t rosqual/icsa22-evaluation:runner "${ROOT_DIR}"
