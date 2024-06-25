0d11235a9b7cd91d7f50928992bae9475e215c0a
Siyan Wu <swu376@gmail.com>
2024-06-25 16:24:18 -0400

    [Autoware-17] Fix prebuild to install python packages

--- experiments/detection/subjects/autoware-17/docker/prebuild.sh
+++ experiments/detection/subjects/autoware-17/docker/prebuild.sh
@@ -1,1 +1,14 @@
+#!/bin/bash
+set -eu
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+
+## install necessary python packages
+pip install \
+  empy==3.3.4 \
+  numpy==1.16.6 \
+  cheetah==2.4.4 \
+  defusedxml==0.6.0 \
+  netifaces==0.10.7
+
 sed -i 's/opencv-3.2.0-dev//' /ros_ws/src/autoware/ros/src/computing/perception/detection/packages/lidar_tracker/CMakeLists.txt
