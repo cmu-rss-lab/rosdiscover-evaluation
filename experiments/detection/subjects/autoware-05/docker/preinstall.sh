7251f28dd6721a35f21706e57e3f0e00835d6138
Tobias Dürschmid <duerschmid@cmu.edu>
2024-06-26 10:14:57 -0400

    Fix autoware-05 preinstall

--- experiments/detection/subjects/autoware-05/docker/preinstall.sh
+++ experiments/detection/subjects/autoware-05/docker/preinstall.sh
@@ -1,1 +1,6 @@
 curl https://bootstrap.pypa.io/pip/2.7/get-pip.py | python2.7
+
+mkdir -p /etc/ros/rosdep/custom
+cp /.dockerinstall/customrule.yaml /etc/ros/rosdep/custom/
+echo "yaml file:///etc/ros/rosdep/custom/customrule.yaml" | sudo tee -a /etc/ros/rosdep/sources.list.d/50-custom.list
+rosdep update
